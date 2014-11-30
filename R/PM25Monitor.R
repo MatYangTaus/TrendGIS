0ccd5fd4af05a35b97e14948aaef8e5c3d9fb60d
ebikei <ebikei@hotmail.com>
2014-11-29 21:37:42 -0500

    Update PM25Monitor.R

--- R/PM25Monitor.R
+++ R/PM25Monitor.R
@@ -16,8 +16,9 @@ PM25_data3$FIPSPOC=paste(PM25_data3$State.Code,PM25_data3$County.Code,PM25_data3
 #PM25_data3$YR=format(PM25_data3$Date.Local,"%Y")
 #PM25_data3$MON=format(PM25_data3$Date.Local,"%m")
 #PM25_data3$YR_Mon=paste(PM25_data3$YR,PM25_data3$MON,sep='-')
+PM25_data3_cut=PM25_data3[PM25_data3$PM25_Value>=0&PM25_data3$PM25_Value<200,]
 Outside_main=c('02','15','72','78')
-PM25_data3_temp=PM25_data3[!(substr(PM25_data3$FIPS,1,2) %in% Outside_main),]
+PM25_data3_temp=PM25_data3_cut[!(substr(PM25_data3_cut$FIPS,1,2) %in% Outside_main),]
 PM25_data4=PM25_data3_temp[PM25_data3_temp$Event.Type=='None'&PM25_data3_temp$Sample.Duration=='24 HOUR',]
 PM25_data5=aggregate(PM25_Value~FIPSPOC+Date.Local,data=PM25_data4,mean)
 
@@ -79,6 +80,67 @@ legend("bottomright",legend=leglabs(round(cuttingpoint,2.0)),bty='n',pch=16,col=
 title=substitute(paste(PM[2.5],' Levels in Autumn (n=',nn,')'),list(nn=sum(!is.na(PM25_data8$Autumn))))
 title(main=list(title,cex=1.9))
 
+dev.off()
+
+
+##GGMAP GIS##GGMAP GIS
+brks=classIntervals(PM25_data4$PM25_Value,n=6)
+cuttingpoint=brks$brks
+USContinental=get_googlemap(c(lon=-96.3,lat=39.75) ,zoom=4, xlim=c(-130,-50), ylim=c(25,50),maptype = "hybrid")
+
+#Winter
+PM25_data8$Value=cut(PM25_data8$Winter,cuttingpoint)
+title=substitute(paste(PM[2.5],' Levels in Winter (n=',nn,')'),list(nn=sum(!is.na(PM25_data8$Winter))))
+Type5=ggmap(USContinental)+
+	geom_point(data=PM25_data8,aes(x=Longitude,y=Latitude,colour=Value),size=2)
+title_legend=expression(PM[2.5]*' Value')
+
+plot1=Type5+
+labs(x='Longitude',y='Latitude',title=title)+
+scale_colour_manual(name=title_legend,values=(brewer.pal(6,"Reds")))+
+theme(legend.title = element_text(colour="Black", size=16, face="bold"))#+
+#guides(colour=FALSE)
+
+#Spring
+PM25_data8$Value=cut(PM25_data8$Spring,cuttingpoint)
+title=substitute(paste(PM[2.5],' Levels in Spring (n=',nn,')'),list(nn=sum(!is.na(PM25_data8$Spring))))
+Type5=ggmap(USContinental)+
+	geom_point(data=PM25_data8,aes(x=Longitude,y=Latitude,colour=Value),size=2)
+title_legend=expression(PM[2.5]*' Value')
+
+plot2=Type5+
+labs(x='Longitude',y='Latitude',title=title)+
+scale_colour_manual(name=title_legend,values=(brewer.pal(6,"Reds")))+
+theme(legend.title = element_text(colour="Black", size=16, face="bold"))#+
+#guides(colour=FALSE)
+
+#Summer
+PM25_data8$Value=cut(PM25_data8$Summer,cuttingpoint)
+title=substitute(paste(PM[2.5],' Levels in Summer (n=',nn,')'),list(nn=sum(!is.na(PM25_data8$Summer))))
+Type5=ggmap(USContinental)+
+	geom_point(data=PM25_data8,aes(x=Longitude,y=Latitude,colour=Value),size=2)
+title_legend=expression(PM[2.5]*' Value')
+
+plot3=Type5+
+labs(x='Longitude',y='Latitude',title=title)+
+scale_colour_manual(name=title_legend,values=(brewer.pal(6,"Reds")))+
+theme(legend.title = element_text(colour="Black", size=16, face="bold"))#+
+#guides(colour=FALSE)
+
+#Autumn
+PM25_data8$Value=cut(PM25_data8$Autumn,cuttingpoint)
+title=substitute(paste(PM[2.5],' Levels in Autumn (n=',nn,')'),list(nn=sum(!is.na(PM25_data8$Autumn))))
+Type5=ggmap(USContinental)+
+	geom_point(data=PM25_data8,aes(x=Longitude,y=Latitude,colour=Value),size=2)
+title_legend=expression(PM[2.5]*' Value')
+
+plot4=Type5+
+labs(x='Longitude',y='Latitude',title=title)+
+scale_colour_manual(name=title_legend,values=(brewer.pal(6,"Reds")))+
+theme(legend.title = element_text(colour="Black", size=16, face="bold"))
+
+pdf(paste(output_location,'PM25MAP_GGMAP.pdf',paste=''),15,10)
+grid.arrange(plot1,plot2,plot3,plot4,ncol=2)
 dev.off()
 
 rm(list=ls())
