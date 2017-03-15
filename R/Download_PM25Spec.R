3c3ecb91ba8f8732785fc9c2fe2cb29d13cf541f
ebikei <ebikei@hotmail.com>
2017-03-15 13:14:54 -0700

    Update Download_PM25Spec.R

--- R/Download_PM25Spec.R
+++ R/Download_PM25Spec.R
@@ -4,12 +4,12 @@ lapply(x, require, character.only=T)
 drive=c("K:\\AirData\\OriginalData")
 setwd(drive)
 
-###################### THIs cODE is ongoing and never work. have to back this code sometime (20160211KE)
+
 ############Download AQS Site
 ######################
 PM25_AQS=data.frame(matrix(nrow=0,ncol=0))
 PM25_Monitor=data.frame(matrix(nrow=0,ncol=0))
-test2=c(1990:2015)
+test2=c(1990:2016)
 
 # Load Parameter List; Pick up species I want to DL.
 ## Note this list is created by downloading all year and take out parameter name.
@@ -36,10 +36,10 @@ ex_list=c('Ambient Max Temperature','Ambient Min Temperature','Ambient Temperatu
 PM25Spec_List_filter=PM25Spec_List[! PM25Spec_List %in% ex_list]
 Outside_main=c('02','15','66','72','78','80')
 
+PM25_Spec_Data=data.frame()
 ptm <- proc.time()
 for (i in 1:length(test2)){  
 	tryCatch({
-i=12
 
 	url=paste("http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/daily_SPEC_",test2[i],".zip",sep='')
 	download.file(url,'temp2.zip')
@@ -51,8 +51,12 @@ i=12
 		filter(!State.Code %in% Outside_main, !Parameter.Name %in% ex_list) %>%
 		mutate(Date=as.Date(as.character(Date.Local),format="%Y-%m-%d"),ParameterName=as.character(Parameter.Name)) %>%
 		select(FIPS_POC,Date,Parameter.Code,ParameterName,Arithmetic.Mean)
-	temp$FIPS_POC[substr(temp$FIPS_POC,1,5)=='12086']=paste('12025',substr(temp$FIPS_POC[substr(temp$FIPS_POC,1,5)=='12086'],6,11),sep='')
 
+	temp$FIPS_POC[substr(temp$FIPS_POC,1,5)=='12086']=paste('12025',substr(temp$FIPS_POC[substr(temp$FIPS_POC,1,5)=='12086'],6,11),sep='')
+	temp=arrange(temp,ParameterName,FIPS_POC,Arithmetic.Mean) %>%
+		mutate(willdelete=paste(ParameterName,FIPS_POC,sep=''))
+	temp$Numbering=sequence(rle(c(temp$willdelete))$lengths)
+	temp=select(temp,-willdelete)
 
 	url2=paste("http://aqsdr1.epa.gov/aqsweb/aqstmp/airdata/annual_all_",test2[i],".zip",sep='')
 	download.file(url2,'temp2.zip')
@@ -63,5 +67,34 @@ i=12
 		FIPS_POC=paste(FIPS,sprintf("%02d",as.numeric(as.character(POC))),sep='')) %>%
 		filter(!State.Code %in% Outside_main, Parameter.Name %in% unique(temp$ParameterName)) %>%
 		mutate(ParameterName=as.character(Parameter.Name),Numb_BelowMDL=Num.Obs.Below.MDL) %>%
-		select(FIPS_POC,Parameter.Code,ParameterName,Numb_BelowMDL)
+		select(FIPS_POC,Parameter.Code,ParameterName,Numb_BelowMDL) %>%
+		arrange(ParameterName,FIPS_POC)
+	temp_mon$FIPS_POC[substr(temp_mon$FIPS_POC,1,5)=='12086']=paste('12025',substr(temp_mon$FIPS_POC[substr(temp_mon$FIPS_POC,1,5)=='12086'],6,11),sep='')
 
+	temp2=left_join(temp,temp_mon,by=c('FIPS_POC','ParameterName')) %>%
+		rename(ParameterCode=Parameter.Code.x) %>%
+		select(-Parameter.Code.y)
+
+	temp3=filter(temp2,Numbering==Numb_BelowMDL) %>%
+		rename(estMDL_value=Arithmetic.Mean) %>%
+		select(FIPS_POC,ParameterName,estMDL_value)
+
+	temp4=left_join(temp2,temp3,by=c('FIPS_POC','ParameterName')) %>%
+		mutate(MDL_Flag=ifelse(Arithmetic.Mean>estMDL_value|Numb_BelowMDL==0,0,1)) 
+	temp4$MDL_Flag[is.na(temp4$MDL_Flag)]=0
+
+	temp4=select(temp4,FIPS_POC,Date,ParameterCode,ParameterName,Arithmetic.Mean,MDL_Flag) %>%
+		rename(Value=Arithmetic.Mean) %>%
+		arrange(FIPS_POC,ParameterName,Date)
+
+	PM25_Spec_Data=rbind(PM25_Spec_Data,temp4)
+	rm(temp,temp2,temp3,temp4,temp_mon)
+
+	}, error=function(e){})
+}
+proc.time() - ptm #This takes about 40min
+
+
+save(PM25_Spec_Data,file="PM25_Species_Data_20160212.RData") #PM25_Spec_Data 
+
+rm(list=ls())
