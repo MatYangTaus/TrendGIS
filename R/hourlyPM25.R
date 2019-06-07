04538afa770d55e9a8b18c7fb82bd982fc3a2b0e
MatYangTaus <ebikei@hotmail.com>
2019-06-07 01:54:04 -0700

    Update hourlyPM25.R

--- R/hourlyPM25.R
+++ R/hourlyPM25.R
@@ -1,17 +1,28 @@
 pacman::p_load(tidyverse, data.table, skimr)
 
 url = "https://aqs.epa.gov/aqsweb/airdata/hourly_88101_2017.zip"
-download.file(url,'C:\\Users\\kebisu\\Downloads\\temp2.zip')
-temp=read.csv(unz('C:\\Users\\kebisu\\Downloads\\temp2.zip',paste("hourly_88101_2017",".csv",sep='')),header=TRUE)
+download.file(url,'C:\\Users\\ebike\\Downloads\\temp2.zip')
+temp=read.csv(unz('C:\\Users\\ebike\\Downloads\\temp2.zip',paste("hourly_88101_2017",".csv",sep='')),header=TRUE)
 
-temp %>%
-	mutate(MDL.Index = ifelse(Sample.Measurement<MDL, 1, 0)) %>%
+temp2 = temp %>%
+	rename(Value = Sample.Measurement) %>%
+	filter(Value >0) %>%
+	mutate(MDL.Index = ifelse(Value<MDL, 1, 0)) %>%
+	filter(State.Code == 6) %>%
+	{.}
+
+temp2 %>%
 	group_by(MDL.Index) %>%
 	count()
 
-temp %>%
-	mutate(MDL.Index = ifelse(Sample.Measurement<MDL, 1, 0)) %>%
-	filter(MDL.Index == 1) %>%
-	group_by(State.Code) %>%
+temp2 %>%
+	filter(MDL.Index == 1,  MDL == 5) %>%
+	mutate(Year = substr(Date.Local, 1, 4)) %>%
+	group_by(County.Name) %>%
 	count() %>%
 	print(n = Inf)
+
+temp3 = temp2 %>%
+	filter(Value >35)  %>%
+
+filter(temp2, County.Name =='Ventura', Date.Local == '2017-12-07')
